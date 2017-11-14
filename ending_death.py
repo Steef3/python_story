64d85321db973f9853c7805403b03bfa88ba70d8
Steef3 <stefan.hiebl@gmx.com>
2017-11-13 23:23:39 -0600

    Display goodie-points at death

--- ending_death.py
+++ ending_death.py
@@ -1,7 +1,7 @@
 from sys import exit
 
 # this print is for testing only
-# make sure that it will be changed to the actual number of goodie_points that the player has
+# TO DO: make sure that it will be changed to the actual number of goodie_points that the player has
 print("Please input a number!")
 goodie_points = input("> ")
 goodie_points = int(goodie_points)
@@ -9,32 +9,34 @@ goodie_points = int(goodie_points)
 # defines how a game ends, in case the player dies
 # system used on every map
 def ending(x):
+    # TO DO: figure out a better way of displaying goodie-points
+    print("You died and you have {} goodie-points!".format(x))
     if x < 100:
         hell()
     elif x >= 100 and x < 500:
         heaven()
     elif x >= 500:
         rebirth()
-    # the following line will not be effective ever, since goodie_points are always an integer
     # python will result in an error, if the goodie_points are anything else than an integer
+    # the following else should never actually run
     else:
         print("You broke the game...")
         exit(0)
 
 # not having enough goodie-points leads to being put into hell
 def hell():
-    print("You died. Since you have not earned enough goodie-points you are going to hell.")
+    print("Since you have not earned enough goodie-points you are going to hell.")
     exit(0)
 
 # having enough goodie-points leads to being allowed into heaven
 def heaven():
-    print("You died. Since you have earned enough goodie-points you are going to heaven!")
+    print("Since you have earned enough goodie-points you are going to heaven!")
     exit(0)
 
 # having more than 499 goodie-points leads to moving on towards a new map, even though the player died
 def rebirth():
-    print("You died. Since you have earned so many goodie-points God grants you another life on a random map!")
-    # map xyz
+    print("Since you have earned so many goodie-points God grants you another life on a random map!")
+    # TO DO: ranom new map xyz
     exit(0)
 
 ending(goodie_points)

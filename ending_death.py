3588c6893d0ec8784872bf6d22d64201e9e75078
Stefan Hiebl <stefan.hiebl@gmx.com>
2017-11-14 21:29:56 -0600

    Continue forest map and implement ending()

--- ending_death.py
+++ ending_death.py
@@ -1,21 +1,24 @@
 from sys import exit
 
-# this print is for testing only
-# TO DO: make sure that it will be changed to the actual number of goodie_points that the player has
-print("Please input a number!")
-goodie_points = input("> ")
-goodie_points = int(goodie_points)
-
 # defines how a game ends, in case the player dies
 # system used on every map
-def ending(x):
+
+# usage: ending(goodie_points)
+def ending():
+
+    # this print is for testing only
+    # TO DO: make sure that it will be changed to the actual number of goodie_points that the player has
+    print("Please input a number!")
+    goodie_points = input("> ")
+    goodie_points = int(goodie_points)
+
     # TO DO: figure out a better way of displaying goodie-points
-    print("You died and you have {} goodie-points!".format(x))
-    if x < 100:
+    print("You died and you have {} goodie-points!".format(goodie_points))
+    if goodie_points < 100:
         hell()
-    elif x >= 100 and x < 500:
+    elif goodie_points >= 100 and goodie_points < 500:
         heaven()
-    elif x >= 500:
+    elif goodie_points >= 500:
         rebirth()
     # python will result in an error, if the goodie_points are anything else than an integer
     # the following else should never actually run
@@ -38,5 +41,3 @@ def rebirth():
     print("Since you have earned so many goodie-points God grants you another life on a random map!")
     # TO DO: ranom new map xyz
     exit(0)
-
-ending(goodie_points)
--- map_1_forest.py
+++ map_1_forest.py
@@ -1,4 +1,5 @@
 # from x import y
+from ending_death import ending
 
 def forest_start():
   print("You are standing in the middle of a forest.")
@@ -17,17 +18,22 @@ def forest_start():
   # TO DO: change all the strings to be independent of lower or capital letters
   if "south" in choice or "South" in choice:
       print("You decided to go towards the South.")
+      south()
 
   elif "north" in choice or "North" in choice:
       print("You decided to go towards the North.")
+      north()
 
   elif "east" in choice or "East" in choice:
       print("You decided to go towards the East.")
+      east()
 
   elif "west" in choice or "West" in choice:
       print("You decided to go towards the West.")
+      west()
 
-  elif "stomp" in choice:
+# TO DO: implement non-case sensitive version for this
+  elif "stomp" in choice or "Stomp" in choice or "ground" in choice or "Ground" in choice:
       print("You decided to stomp more.")
       print("That was a risky idea!!! That one last stomp was too much for the ground and you fall deep down into a cave.")
       print("Destroying ground (especially if it's not yours) is very bad! - 5 goodie-points.")
@@ -37,13 +43,23 @@ def forest_start():
 
       # TO DO: figure out how to implement indentation for each decision that is needed
       print("""Each chest has something written on it:
-      'Inspirational quote 1'
-      'Inspirational quote 2'
-      'Inspirational quote 3'""")
+      1. 'Choose a job you love, and you will never have to work a day in your life.' - Confucius
+      2. 'Love all, trust a few, do wrong to none.' - William Shakespeare
+      3. 'Happiness is not something ready made. It comes from your own actions.' - Dalai Lama""")
       print("Which chest do you open?")
 
   else:
       print("You typed in something the game does not understand and that made it explode.")
-      # TO DO: implement ending()
+      # WARNING: make sure to change, when goodie-points are actually implemented
+      ending()
+
+def south():
+    print("South Test.")
+def north():
+    print("North Test.")
+def east():
+    print("East Test.")
+def west():
+    print("West Test.")
 
 forest_start()

2fa177e4cc7db98870867a855d5b5cab3c9567b6
Stefan Hiebl <stefan.hiebl@gmx.com>
2017-11-17 01:02:11 -0600

    Add goodie-points reset after death

--- ending_death.py
+++ ending_death.py
@@ -20,20 +20,33 @@ def ending():
     # the following else should never actually run
     else:
         print("You broke the game...")
+        reset_goodie_points()
         exit(0)
 
 # not having enough goodie-points leads to being put into hell
 def hell():
     print("Since you have not earned enough goodie-points you are going to hell.")
+    reset_goodie_points()
     exit(0)
 
 # having enough goodie-points leads to being allowed into heaven
 def heaven():
     print("Since you have earned enough goodie-points you are going to heaven!")
+    reset_goodie_points()
     exit(0)
 
 # having more than 499 goodie-points leads to moving on towards a new map, even though the player died
 def rebirth():
     print("Since you have earned so many goodie-points God grants you another life on a random map!")
-    # TO DO: ranom new map xyz
+    # TO DO: random new map xyz
+    reset_goodie_points()
     exit(0)
+
+def reset_goodie_points():
+    with open('goodie_points.py', 'r') as goodie_file:
+        data = goodie_file.readlines()
+        # TO DO: figure out an alternative way of doing this, because it is very static this way
+        data[1] = 'goodie_points_count = 0' + '\n'
+
+    with open('goodie_points.py', 'w') as goodie_file:
+        goodie_file.writelines(data)
